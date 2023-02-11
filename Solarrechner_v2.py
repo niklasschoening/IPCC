c5edba9cb99404348e053fdb3bbeb77504f60a13
niklasschoening <115229573+niklasschoening@users.noreply.github.com>
2023-02-11 09:57:19 +0100

    Add files via upload

---
+++ Solarrechner_v2.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+size_y = 450
+size_x = 900
+
+class c_Label:
+    def __init__(self, screen, name, text, bd, width, row, column):
+        self.screen = screen
+        self.name = name
+        self.text = text
+        self.bd = bd
+        self.width = width
+        self.row = row
+        self.column = column
+
+        self.label = Label(self.screen, text=self.text)
+        self.input = Entry(window, bd=self.bd, width=self.width)
+
+        self.label.grid(row=self.row, column=self.column)
+        self.input.grid(row=self.row, column=self.column+1)
+
+window = Tk()
+window.geometry(f"{size_x}x{size_y}")
+
+amount_label = c_Label(window, "amount", "Menge Solarpanele:", 5, 2, 0, 0)
+
+window.mainloop()

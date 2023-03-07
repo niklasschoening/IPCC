e7d7d47856fd37df6b643bc15ed2430773e94595
Niklasschoening <Sch200607@gmail.com>
2023-03-07 11:26:10 +0100

    Optimieren

--- Solarrechner_v2.py
+++ Solarrechner_v2.py
@@ -3,15 +3,18 @@ from tkinter import *
 size_y = 450
 size_x = 900
 
-class c_Label:
-    def __init__(self, screen, name, text, bd, width, row, column):
+class classWidget:
+    def __init__(self, screen, bd, row, column):
         self.screen = screen
-        self.name = name
-        self.text = text
         self.bd = bd
-        self.width = width
         self.row = row
         self.column = column
+
+class classLabel(classWidget):
+    def __init__(self, screen, text, bd, width, row, column):
+        super().__init__(screen, bd, row, column)
+        self.text = text
+        self.width = width
 
         self.label = Label(self.screen, text=self.text)
         self.input = Entry(window, bd=self.bd, width=self.width)
@@ -19,22 +22,32 @@ class c_Label:
         self.label.grid(row=self.row, column=self.column)
         self.input.grid(row=self.row, column=self.column+1)
 
-class c_Button:
-    def __init__(self, screen, name, text, func, bd, width, row, column):
-        self.screen = screen
-        self.name = name
+class classButton(classWidget):
+    def __init__(self, screen, text, func, bd, row, column):
+        super().__init__(screen, bd, row, column)
         self.text = text
         self.func = func
-        self.bd = bd
-        self.width = width
-        self.row = row
-        self.column = column
 
-        
+        self.button = Button(self.screen, text = self.text, bd=self.bd, command=self.func)
+
+        self.button.grid(row=self.row, column=self.column)
+
+def setBackground(file):
+    """Hintergrundbild setzen"""
+    filename = PhotoImage(file = "Assets/Background_1.png")
+    background_label = Label(image=filename)
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+def calculate():
+    """Berechnung der Werte"""   
 
 window = Tk()
 window.geometry(f"{size_x}x{size_y}")
+window.title("Solarrechner [Niklas, Marco, Cilia, Sarah, Nico]")
+window.minsize(600, 300)
+setBackground(None)
 
-amount_label = c_Label(window, "amount", "Menge Solarpanele:", 5, 2, 0, 0)
+amountlabel = classLabel(window, "Menge Solarpanele:", 2, 2, 0, 0)
+calculateButton = classButton(window, "berechnen", calculate, 2, 1, 1)
 
 window.mainloop()

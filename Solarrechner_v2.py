b75376d8525a0abc1cdb90e08fcbd0ef9d27a7e8
Niklasschoening <Sch200607@gmail.com>
2023-03-14 11:27:33 +0100

    optisches Verbessern

--- Solarrechner_v2.py
+++ Solarrechner_v2.py
@@ -1,7 +1,5 @@
 from tkinter import *
-
-size_y = 450
-size_x = 900
+from PIL import ImageTk, Image
 
 class classWidget:
     def __init__(self, screen, bd, row, column):
@@ -11,43 +9,42 @@ class classWidget:
         self.column = column
 
 class classLabel(classWidget):
-    def __init__(self, screen, text, bd, width, row, column):
-        super().__init__(screen, bd, row, column)
+    def __init__(self, screen, text, bd, width, x, y):
+        super().__init__(screen, bd, x, y)
         self.text = text
         self.width = width
 
         self.label = Label(self.screen, text=self.text)
         self.input = Entry(window, bd=self.bd, width=self.width)
 
-        self.label.grid(row=self.row, column=self.column)
-        self.input.grid(row=self.row, column=self.column+1)
+        self.label.grid(row = self.row, column = self.column)
+        self.input.grid(row = self.row, column = self.column)
 
 class classButton(classWidget):
-    def __init__(self, screen, text, func, bd, row, column):
-        super().__init__(screen, bd, row, column)
+    def __init__(self, screen, text, func, bd, x, y):
+        super().__init__(screen, bd, x, y)
         self.text = text
         self.func = func
 
         self.button = Button(self.screen, text = self.text, bd=self.bd, command=self.func)
 
-        self.button.grid(row=self.row, column=self.column)
-
-def setBackground(file):
-    """Hintergrundbild setzen"""
-    filename = PhotoImage(file = "Assets/Background_1.png")
-    background_label = Label(image=filename)
-    background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.button.grid(row = self.row, column = self.column)
 
 def calculate():
-    """Berechnung der Werte"""   
+    """Berechnung der Werte"""
 
 window = Tk()
-window.geometry(f"{size_x}x{size_y}")
 window.title("Solarrechner [Niklas, Marco, Cilia, Sarah, Nico]")
-window.minsize(600, 300)
-setBackground(None)
+window.geometry("400x400")
 
-amountlabel = classLabel(window, "Menge Solarpanele:", 2, 2, 0, 0)
-calculateButton = classButton(window, "berechnen", calculate, 2, 1, 1)
+#------Background-------  
+bg = PhotoImage(file = "Assets/Background_1.png")
+canvas1 = Canvas(window, width = 400, height = 400)
+canvas1.grid(row = 0, column = 0)
+canvas1.create_image( 0, 0, image = bg, anchor = "nw")
+#------Background-------
+
+amountlabel = classLabel(window, "Menge Solarpanele:", 1, 3, 3, 0)
+calculateButton = classButton(window, "berechnen", calculate, 1, 0, 0)
 
 window.mainloop()
